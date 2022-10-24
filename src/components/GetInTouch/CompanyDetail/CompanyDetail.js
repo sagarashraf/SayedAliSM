b3ba78dc76f14e7ebf1b7c170d2fd16129c7782d
sagarashraf93 <sagarashraf93@gmail.com>
2022-10-24 23:45:44 +0500

    done

--- src/components/GetInTouch/CompanyDetail/CompanyDetail.js
+++ src/components/GetInTouch/CompanyDetail/CompanyDetail.js
@@ -22,112 +22,110 @@ import "./CompanyDetail.css";
 export const CompanyDetail = (props) => {
 	return (
 		<Container fluid>
-			<Container fluid>
-				<div className='mb-3 text-center'>
-					<img
-						width={60}
-						height={120}
-						alt='logo'
-						src={require("../../../assets/img/logo-tower.png")}></img>
-					<h3 className='way-to-us'>Ways to Reach us.</h3>
-					<p>Feel free to get in touch with us</p>
-				</div>
-				<Row>
-					<ListGroup variant='flush'>
-						<ListGroup.Item className='mb-3'>
-							{" "}
-							<Row>
-								<Col lg={12}>
-									<div className='companydetail-text-style'>
-										{" "}
-										<FontAwesomeIcon
-											className='companyDetail-icon'
-											icon={faPhone}
-										/>{" "}
-										+932-43534-244
-									</div>
-								</Col>
-								{/* <Col lg={12}>
-									<div className='companydetail-text-style'>+932-43534-244</div>
-								</Col> */}
-							</Row>
-						</ListGroup.Item>
-						<ListGroup.Item className='mb-3'>
-							<Row>
-								<Col lg={12}>
-									<div className='companydetail-text-style'>
-										{" "}
-										<FontAwesomeIcon
-											className='companyDetail-icon'
-											icon={faEnvelope}
-										/>{" "}
-										ABC@smpayouts.com
-									</div>
-								</Col>
-								{/* <Col lg={12}>
-									<div className='companydetail-text-style'>
-										ArslanABC@smarterpayouts.com
-									</div>
-								</Col> */}
-							</Row>
-						</ListGroup.Item>
-						<ListGroup.Item className='mb-3'>
-							<Row>
-								<Col lg={12}>
-									<div className='companydetail-text-style'>
-										{" "}
-										<FontAwesomeIcon
-											style={{ marginRight: "6px" }}
-											className='companyDetail-icon'
-											icon={faMapMarker}
-										/>{" "}
-										76500E kotlakhpat, Model Town , S-block Lahore , Pakistan
-									</div>
-								</Col>
-								{/* <Col lg={12}>
-									<div className='companydetail-text-style px-2'>
-										{"     "}76500E kotlakhpat, Model Town , S-block Lahore ,
-										Pakistan
-									</div>
-								</Col> */}
-							</Row>
-						</ListGroup.Item>
-						<ListGroup.Item className='mb-3'>
-							<Row>
-								<Col lg={12}>
-									<div className='companydetail-text-style'>
-										{" "}
-										<FontAwesomeIcon
-											className='companyDetail-icon'
-											icon={faShieldHalved}
-										/>{" "}
-										Privacy Policy
-										{/* <div className='companydetail-text-style'>
-											<u>Visit here</u>
-										</div> */}
-									</div>
-								</Col>
-							</Row>
-							<div className='ms-2 me-auto'></div>
-						</ListGroup.Item>
-						<ListGroup.Item className='mb-3'>
+			<div className='mb-3 text-center'>
+				<img
+					width={60}
+					height={120}
+					alt='logo'
+					src={require("../../../assets/img/logo-tower.png")}></img>
+				<h3 className='way-to-us'>Ways to Reach us.</h3>
+				<p>Feel free to get in touch with us</p>
+			</div>
+			<Row>
+				<ListGroup variant='flush'>
+					<ListGroup.Item className='mb-3'>
+						{" "}
+						<Row>
 							<Col lg={12}>
 								<div className='companydetail-text-style'>
 									{" "}
 									<FontAwesomeIcon
 										className='companyDetail-icon'
-										icon={faFilePen}
+										icon={faPhone}
 									/>{" "}
-									Terms and Conditions
+									+932-43534-244
+								</div>
+							</Col>
+							{/* <Col lg={12}>
+									<div className='companydetail-text-style'>+932-43534-244</div>
+								</Col> */}
+						</Row>
+					</ListGroup.Item>
+					<ListGroup.Item className='mb-3'>
+						<Row>
+							<Col lg={12}>
+								<div className='companydetail-text-style'>
+									{" "}
+									<FontAwesomeIcon
+										className='companyDetail-icon'
+										icon={faEnvelope}
+									/>{" "}
+									ABC@smpayouts.com
+								</div>
+							</Col>
+							{/* <Col lg={12}>
+									<div className='companydetail-text-style'>
+										ArslanABC@smarterpayouts.com
+									</div>
+								</Col> */}
+						</Row>
+					</ListGroup.Item>
+					<ListGroup.Item className='mb-3'>
+						<Row>
+							<Col lg={12}>
+								<div className='companydetail-text-style'>
+									{" "}
+									<FontAwesomeIcon
+										style={{ marginRight: "6px" }}
+										className='companyDetail-icon'
+										icon={faMapMarker}
+									/>{" "}
+									76500E kotlakhpat, Model Town , S-block Lahore , Pakistan
+								</div>
+							</Col>
+							{/* <Col lg={12}>
+									<div className='companydetail-text-style px-2'>
+										{"     "}76500E kotlakhpat, Model Town , S-block Lahore ,
+										Pakistan
+									</div>
+								</Col> */}
+						</Row>
+					</ListGroup.Item>
+					<ListGroup.Item className='mb-3'>
+						<Row>
+							<Col lg={12}>
+								<div className='companydetail-text-style'>
+									{" "}
+									<FontAwesomeIcon
+										className='companyDetail-icon'
+										icon={faShieldHalved}
+									/>{" "}
+									Privacy Policy
 									{/* <div className='companydetail-text-style'>
 											<u>Visit here</u>
 										</div> */}
 								</div>
 							</Col>
-						</ListGroup.Item>
-					</ListGroup>
-				</Row>
-			</Container>
+						</Row>
+						<div className='ms-2 me-auto'></div>
+					</ListGroup.Item>
+					<ListGroup.Item className='mb-3'>
+						<Col lg={12}>
+							<div className='companydetail-text-style'>
+								{" "}
+								<FontAwesomeIcon
+									className='companyDetail-icon'
+									icon={faFilePen}
+								/>{" "}
+								Terms and Conditions
+								{/* <div className='companydetail-text-style'>
+											<u>Visit here</u>
+										</div> */}
+							</div>
+						</Col>
+					</ListGroup.Item>
+				</ListGroup>
+			</Row>
 		</Container>
 	);
 };
